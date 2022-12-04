ed742ceb6c5703e0b1e19cf8446397aee13f491c
DilmurodjonT <dilmurod_tadjibaev@mail.ru>
2022-12-04 16:35:18 +0500

    has been filled

--- scrit.js
+++ scrit.js
@@ -1,6 +1,6 @@
 var nam = prompt("ismingizni kiriting");
 var sorov = Number(prompt(`${nam} qancha pulingiz bor?`));
-if (sorov >= 9795192.3) {
+if (sorov >= 11100.61 * 250 + 11100.61 * 500 + 12247.79 * 120) {
   alert(`Oq yo'l ${nam}!`);
 } else {
   alert(`${nam} ozgina sabir qilish kerak ekan.`);
