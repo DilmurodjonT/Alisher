9f6937f2fcf879aaa206b85af7ef80478862d5f9
DilmurodjonT <dilmurod_tadjibaev@mail.ru>
2022-12-04 15:06:49 +0500

    first commit

---
+++ scrit.js
@@ -0,0 +1,7 @@
+var nam = prompt("ismingizni kiriting");
+var sorov = Number(prompt(`${nam} qancha pulingiz bor?`));
+if (sorov >= 9795192.3) {
+  alert(`Oq yo'l ${nam}!`);
+} else {
+  alert(`${nam} ozgina sabir qilish kerak ekan.`);
+}
